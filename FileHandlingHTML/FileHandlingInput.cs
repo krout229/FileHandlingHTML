b4e0c8aaaf8d91b83528c1f914abd4503c1ea270
Ladly Rout <krout229.kr@gmail.com>
2022-04-01 18:32:35 +0530

    Add project files.

---
+++ FileHandlingHTML/FileHandlingInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileHandlingHTML
+{
+    class FileHandlingInput
+    {
+        public void ReadFile()
+        {
+            FileStream fileStream = new FileStream(@"C:\Training\mydata.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(fileStream);
+            int count = 0;
+            while (reader.Peek() > 0)
+            {
+                string line = reader.ReadLine();
+                if (line.Contains("input"))
+                    count++;
+            }
+            Console.WriteLine("Count of  input " + count);
+            reader.Close();
+            fileStream.Close();
+        }
+    }
+}
